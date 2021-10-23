372655fd8d62ef6f0e2733388e7bb84cf65f8223
Az-s <azim.sa@yahoo.com>
2021-10-23 15:47:17 +0600

    created comments and news essence

---
+++ backend/app/comments.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const {nanoid} = require('nanoid');
+const config = require('../config');
+const mysqlDb = require('../mysqlDb');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, config.uploadPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, nanoid() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({storage});
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const [products] = await mysqlDb.getConnection().query('SELECT * FROM ??', ['products']);
+  res.send(products);
+});
+
+router.get('/:id', async (req, res) => {
+  const [product] = await mysqlDb.getConnection().query(
+		`SELECT * FROM ?? where id = ?`,
+	  ['products', req.params.id])
+  if (!product) {
+    return res.status(404).send({error: 'Product not found'});
+  }
+
+  res.send(product[0]);
+});
+
+router.post('/', upload.single('image'), async (req, res) => {
+  if (!req.body.author || !req.body.comment) {
+    return res.status(400).send({error: 'Data not valid'});
+  }
+
+  const comment = {
+    author: req.body.author ? req.body.author : 'Anonymous',
+    comment: req.body.comment,
+  };
+
+  if (req.file) {
+    comment.image = req.file.filename;
+  }
+
+  const newComment = await mysqlDb.getConnection().query(
+		'INSERT INTO ?? (author, comment, image) values (?, ?, ?, ?)',
+	  ['products', comment.author, comment.comment, comment.image]
+  );
+
+  res.send({
+	  ...comment,
+	  id: newComment.insertId
+  });
+});
+
+router.put('/:id', upload.single('image'), async (req, res) => {
+	const product = {
+		title: req.body.title,
+		price: req.body.price,
+		description: req.body.description,
+	};
+	
+	if(req.file) product.image = req.file.filename;
+	
+	await mysqlDb.getConnection().query(
+		'UPDATE ?? SET ? where id = ?',
+		['products', {...product}, req.params.id]);
+	
+	res.send({message: `Update successful, id= ${req.params.id}`});
+});
+
+module.exports = router;
