861426b206b0ea25f79461cc5589cbede0b67e22
Az-s <azim.sa@yahoo.com>
2021-10-23 13:19:25 +0600

    add newPost component

--- frontend/src/container/NewPosts/NewPosts.js
+++ frontend/src/container/NewPosts/NewPosts.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Typography } from '@mui/material';
+import PostForm from "../../components/PostForm/PostForm";
 
-const NewPosts = () => {
+const NewPosts = ({ onSubmit }) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Typography variant="h4">New post</Typography>
+            <PostForm onSubmit={onSubmit} />
+        </>
     )
 }
 
